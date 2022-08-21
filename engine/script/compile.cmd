5d7114ede19db1a75087618a15e356840d0093e5
aoineko <aoineko@free.fr>
2022-08-21 21:45:31 +0200

    [BuildTool] Fixed CDB debug file generation

--- engine/script/compile.cmd
+++ engine/script/compile.cmd
@@ -52,7 +52,7 @@ if /I %Optim%==Speed ( set AddOpt=!AddOpt! --opt-code-speed )
 if /I %Optim%==Size ( set AddOpt=!AddOpt! --opt-code-size )
 if not "%2"=="" ( set AddOpt=!AddOpt! --code-size %2 )
 if not "%3"=="" ( set AddOpt=!AddOpt! --codeseg SEG%3 )
-
+if %Debug%==1 ( set AddOpt=!AddOpt! --debug )
 
 REM set AddOpt=!AddOpt! --max-allocs-per-node 100000
 REM set AddOpt=!AddOpt! --constseg RODATA
