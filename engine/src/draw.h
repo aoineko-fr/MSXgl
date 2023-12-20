64d3253285df25c73c2cd5c1d96e9d7dfbd29d6a
Pedro de Medeiros <pedro.medeiros@gmail.com>
2023-12-20 17:58:18 -0300

    draw* docs updated

--- engine/src/draw.h
+++ engine/src/draw.h
@@ -21,81 +21,81 @@
 //-----------------------------------------------------------------------------
 
 // Function: Draw_Line
-// Draw a line (pixel unit)
+// Draws a line (one pixel wide)
 //
 // Parameters:
 //   x1 - Source position X
 //   y1 - Source position Y
 //   x2 - Destination position X
 //   y2 - Destination position Y
-//   color - Draw color (format depends of the screen mode)
+//   color - Draw color (format depends on screen mode)
 //   op - Draw operator (VDP_OP_AND, VDP_OP_OR, ...)
 void Draw_Line(UX x1, UY y1, UX x2, UY y2, u8 color, u8 op);
 
 // Function: Draw_LineH
-// Draw an horizontal line (pixel unit)
+// Draws an horizontal line (one pixel wide)
 //
 // Parameters:
 //   x1 - Source position X
 //   x2 - Destination position X
 //   y - Position Y
-//   color - Draw color (format depends of the screen mode)
+//   color - Draw color (format depends on screen mode)
 //   op - Draw operator (VDP_OP_AND, VDP_OP_OR, ...)
 void Draw_LineH(UX x1, UX x2, UY y, u8 color, u8 op);
 
 // Function: Draw_LineV
-// Draw a vertical line (pixel unit)
+// Draws a vertical line (one pixel wide)
 //
 // Parameters:
 //   x - Position X
 //   y1 - Source position Y
 //   y2 - Destination position Y
-//   color - Draw color (format depends of the screen mode)
+//   color - Draw color (format depends on screen mode)
 //   op - Draw operator (VDP_OP_AND, VDP_OP_OR, ...)
 void Draw_LineV(UX x, UY y1, UY y2, u8 color, u8 op);
 
 // Function: Draw_Box
-// Draw a box (pixel unit)
+// Draws a box (one pixel wide)
 //
 // Parameters:
 //   x1 - Source position X
 //   y1 - Source position Y
 //   x2 - Destination position X
 //   y2 - Destination position Y
-//   color - Draw color (format depends of the screen mode)
+//   color - Draw color (format depends on screen mode)
 //   op - Draw operator (VDP_OP_AND, VDP_OP_OR, ...)
 void Draw_Box(UX x1, UY y1, UX x2, UY y2, u8 color, u8 op);
 
 // Function: Draw_FillBox
-// Draw a full filled box (pixel unit)
+// Draws a fully filled box
 //
 // Parameters:
 //   x1 - Source position X
 //   y1 - Source position Y
 //   x2 - Destination position X
 //   y2 - Destination position Y
-//   color - Draw color (format depends of the screen mode)
+//   color - Draw color (format depends on screen mode)
 //   op - Draw operator (VDP_OP_AND, VDP_OP_OR, ...)
 void Draw_FillBox(UX x1, UY y1, UX x2, UY y2, u8 color, u8 op);
 
 // Function: Draw_Circle
-// Draw a circle (pixel unit)
+// Draws a circle (one pixel wide)
 //
 // Parameters:
 //   x - Destination position
 //   y - Destination position
 //   radius - Circle radius
-//   color - Draw color (format depends of the screen mode)
+//   color - Draw color (format depends on screen mode)
 //   op - Draw operator (VDP_OP_AND, VDP_OP_OR, ...)
 void Draw_Circle(UX x, UY y, u8 radius, u8 color, u8 op);
 
 // Function: Draw_Point
-// Draw a point
+// Draws a point
 //
 // Parameters:
 //   x - Destination position
 //   y - Destination position
-//   color - Draw color (format depends of the screen mode)
+//   color - Draw color (format depends on screen mode)
 //   op - Draw operator (VDP_OP_AND, VDP_OP_OR, ...)
 inline void Draw_Point(UX x, UY y, u8 color, u8 op) { VDP_CommandPSET(x, y, color, op); }
 
