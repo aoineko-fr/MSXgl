c0834fcfdc44867afa092b9f33d57d98d550b534
Pedro de Medeiros <pedro.medeiros@gmail.com>
2025-02-16 11:22:46 -0300

    Since PR#45 is not an option, this hack solves it
    This makes build.sh works out of the box, even with DOS "\r\n" newlines.

--- projects/samples/build.sh
+++ projects/samples/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env -S bash
 # ____________________________
 # ██▀▀█▀▀██▀▀▀▀▀▀▀█▀▀█        │   ▄▄▄                ▄▄
 # ██  ▀  █▄  ▀██▄ ▀ ▄█ ▄▀▀ █  │  ▀█▄  ▄▀██ ▄█▄█ ██▀▄ ██  ▄███
@@ -6,10 +6,10 @@
 # ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀────────┘                 ▀▀
 #  by Guillaume 'Aoineko' Blanchard under CC BY-SA license
 #────────────────────────────────────────────────────────────────────
-clear
-
-if type -P node; then
-	node ../../engine/script/js/build.js projname=$1 target=$2
-else
-	../../tools/build/Node/node ../../engine/script/js/build.js projname=$1 target=$2
-fi
+clear                                                                                        #
+                                                                                             #
+if type -P node; then                                                                        #
+	node ../../engine/script/js/build.js projname=$1 target=$2                           #
+else                                                                                         #
+	../../tools/build/Node/node ../../engine/script/js/build.js projname=$1 target=$2    #
+fi                                                                                           #
