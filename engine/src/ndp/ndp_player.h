fcd15a985b2257f23a728f5ab820455db48e6e53
Aoineko <aoineko@free.fr>
2026-03-26 14:09:09 +0100

    [NDP] Moved variables to C code (WIP)

--- engine/src/ndp/ndp_player.h
+++ engine/src/ndp/ndp_player.h
@@ -112,7 +112,8 @@ typedef struct NDP_WorkArea
 #define NDP_CHNUM		4		// Number of channels used
 
 // NDP work area (defined in NDP_WRK.ASM)
-extern u8   g_NDP_CH1WRK[NDP_WSIZE];	// Rhythm track
+extern u8   g_NDP_CH1WRK[33];	// Rhythm track
+extern u8   g_NDP_RVOLW[28];
 extern NDP_WorkArea   g_NDP_CH2WRK;	// Standard track 1
 extern NDP_WorkArea   g_NDP_CH3WRK;	// Standard track 2
 extern NDP_WorkArea   g_NDP_CH4WRK;	// Standard track 3
@@ -160,7 +161,8 @@ extern u8   g_NDP_MIXWRK;			// 10NNNTTT 0:ON/1:OFF
 extern u8   g_NDP_MIXWRS;			// Sound effects mixing work
 extern u16  g_NDP_HENVPW;			// Hard envelope period
 extern u16  g_NDP_BGMADR;			// Song data start address
-extern u16  g_NDP_PTABLE[12 * 8];	// Pitch table
+extern u16  g_NDP_PTABLE[12*3];	// Pitch table
+extern u16  g_NDP_O4C[12*5];
 
 // Address of the SFX data
 extern const u8* g_NDP_SFXData;
